8396b3aaed18143e3e60a6e952330af3180fa1ab
yanghui <yh@xlh-tech.com>
2016-08-17 14:46:35 +0800

    页面框架搭建。

--- PFTSDesktop/MainWindow.xaml.cs
+++ PFTSDesktop/MainWindow.xaml.cs
@@ -13,13 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using PFTSModel;
+using PftcsUITemplate.Controls;
 
 namespace PFTSDesktop
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : WindowTemplet
     {
         public MainWindow()
         {
@@ -42,6 +43,81 @@ namespace PFTSDesktop
             {
                 MessageBox.Show("插入失败");
             }
+        }
+
+        private void MainWindow_OnMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton != MouseButton.Left) return;
+            if (this.ResizeMode == ResizeMode.NoResize) return;
+
+            //判断点击的控件不是Grid类型，就可以双击
+            if (!(e.OriginalSource is System.Windows.Controls.Image))
+            {
+                return;
+            }
+
+
+            IInputElement feSource = e.MouseDevice.DirectlyOver;
+
+
+            Point pt = e.MouseDevice.GetPosition(this);
+            if (pt.Y < MoveHeight)
+            {
+                if (this.WindowState == WindowState.Normal)
+                {
+                    this.WindowState = WindowState.Maximized;
+                    this.btnNomalMax.Visibility = Visibility.Hidden;
+                    this.btnMax.Visibility = Visibility.Visible;
+                }
+                else if (this.WindowState == WindowState.Maximized)
+                {
+                    this.WindowState = WindowState.Normal;
+                    this.btnNomalMax.Visibility = Visibility.Visible;
+                    this.btnMax.Visibility = Visibility.Hidden;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 窗口最小化
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSysMin_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        /// <summary>
+        /// 关闭当前程序
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSysExit_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        /// <summary>
+        /// 窗口最大化
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSysMax_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+            {
+                this.WindowState = WindowState.Maximized;
+                this.btnNomalMax.Visibility = Visibility.Hidden;
+                this.btnMax.Visibility = Visibility.Visible;
+            }
+            else if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+                this.btnNomalMax.Visibility = Visibility.Visible;
+                this.btnMax.Visibility = Visibility.Hidden;
+            }
+
         }
     }
 }
---
+++ PftcsUITemplate/Controls/TabItemTemplet.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Media;
+
+namespace PftcsUITemplate.Controls
+{
+    public class TabItemTemplet : TabItem
+    {
+        public static readonly DependencyProperty MyMoverBrushProperty;
+		public static readonly DependencyProperty MyEnterBrushProperty;
+		public Brush MyMoverBrush
+		{
+			get
+			{
+                return base.GetValue(TabItemTemplet.MyMoverBrushProperty) as Brush;
+			}
+			set
+			{
+				base.SetValue(TabItemTemplet.MyMoverBrushProperty, value);
+			}
+		}
+		public Brush MyEnterBrush
+		{
+			get
+			{
+				return base.GetValue(TabItemTemplet.MyEnterBrushProperty) as Brush;
+			}
+			set
+			{
+				base.SetValue(TabItemTemplet.MyEnterBrushProperty, value);
+			}
+		}
+		static TabItemTemplet()
+		{
+			TabItemTemplet.MyMoverBrushProperty = DependencyProperty.Register("MyMoverBrush", typeof(Brush), typeof(TabItemTemplet), new PropertyMetadata(null));
+			TabItemTemplet.MyEnterBrushProperty = DependencyProperty.Register("MyEnterBrush", typeof(Brush), typeof(TabItemTemplet), new PropertyMetadata(null));
+			FrameworkElement.DefaultStyleKeyProperty.OverrideMetadata(typeof(TabItemTemplet), new FrameworkPropertyMetadata(typeof(TabItemTemplet)));
+		}
+    }
+}
