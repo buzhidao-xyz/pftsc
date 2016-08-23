cea8d47bf570cd22afa0e9a83f6122bc9662b500
ilisin <ggt@xlh-tech.com>
2016-08-23 15:47:10 +0800

    添加rfid天线图标

---
+++ PFTSScene/PFTS3DSceneControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PFTSScene
+{
+    /// <summary>
+    /// PFTS3DSceneControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class PFTS3DSceneControl : UserControl
+    {
+        public PFTS3DSceneControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
