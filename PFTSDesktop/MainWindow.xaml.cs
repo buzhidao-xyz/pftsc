93135f9dc595b22501a1da8d0a79bcc6be66e442
yanghui <yh@xlh-tech.com>
2016-10-13 11:22:47 +0800

    左侧导航栏伸缩图标修改。

--- PFTSDesktop/MainWindow.xaml.cs
+++ PFTSDesktop/MainWindow.xaml.cs
@@ -193,12 +193,12 @@ namespace PFTSDesktop
             if (rowEdit.Width == new GridLength(150))
             {
                 rowEdit.Width = new GridLength(0);
-                btnZoom.Content="》";
+                btnZoom.RenderTransform = new RotateTransform(-90);
             }
             else
             {
                 rowEdit.Width = new GridLength(150);
-                btnZoom.Content = "《";
+                btnZoom.RenderTransform = new RotateTransform(90);
             }
         }
     }
